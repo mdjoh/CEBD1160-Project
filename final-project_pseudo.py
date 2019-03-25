b2e951a44487a8d10154ffd0d253340b09497fe4
mdjoh <37449634+mdjoh@users.noreply.github.com>
2019-03-25 17:53:30 -0400

    Delete final-project_pseudo.py

--- final-project_pseudo.py
+++
@@ -1,9 +0,0 @@
-# Convert np array to pd dataframe
-#
-# Correlation matrix plotting
-# scatter plots of Y vs. features to see how data are distributed
-# See which ones to include in regression model via PCA
-#
-# Do linear regression
-# machine learning - split dataset into training and testing sets
-# train and test model
